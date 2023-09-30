3a4d5e269183f922e13f5124a6c9782d5a4f5e11
PublicRouter <jkggaringer@gmail.com>
2023-09-30 17:36:50 -0500

    creating base project directory structure; vite frontend using react; backend MVC architecture setup with express, dotenv, and xrpl dependencies

---
+++ sigVerify-frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+import PrototypeUi from './components/PrototypeUi/PrototypeUI'
+
+function App() {
+  return (
+     <PrototypeUi />
+  )
+}
+
+export default App
---
+++ sigVerify-frontend/src/components/PrototypeUi/PrototypeUi.jsx
@@ -0,0 +1,14 @@
+import './PrototypeUi.css'
+
+function PrototypeUi() {
+  return (
+      <div id="protoContainer">
+        <h1>Welcome to SigVerify!</h1>
+        <div id="section-one">
+
+        </div>
+      </div>
+  )
+}
+
+export default PrototypeUi
