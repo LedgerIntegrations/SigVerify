f59bfe1c4d2d9e7012fa322b9e217edadb1c9f3c
PublicRouter <jkggaringer@gmail.com>
2023-10-11 16:31:42 -0500

    adding loader screen, editing home page buttons to 'sign in' and 'create account' to open it up to more than just xrpl wallet sign-in

--- sigVerify-frontend/src/components/HomePage/HomePage.jsx
+++ sigVerify-frontend/src/components/HomePage/HomePage.jsx
@@ -2,19 +2,24 @@ import './HomePage.css'
 import React from 'react';
 import { Link } from 'react-router-dom';
 import computerAsset from './assets/fingerprintlike_computer_midj.png';
-
+import logoImage from '../Navigation/assets/svLogo.png';
+import LandingLoader from '../LandingLoader/LandingLoader';
 function HomePage() {
     return (
         <div id="home-page-container">
+            <LandingLoader />
             <div id="top-section">
                 <h2>Sig Verify</h2>
-                <img id="home-image" src={computerAsset} />
+                <img id="home-image" src={logoImage} />
                 <p id="call-to-action-text">Signed, Scanned, <br/> Delivered.</p>
                 <p id="call-to-action-smaller">Premier document signing experience.</p>
             </div>
             <div id="bottom-section">
                 <Link to="/login" >
-                    <button className='buttonPop'>Connect Wallet</button>
+                    <button className='buttonPop'>Sign In</button>
+                </Link>
+                <Link to="/login" >
+                    <button className='buttonPop'>Create Account</button>
                 </Link>
             </div>
         </div>
---
+++ sigVerify-frontend/src/components/LandingLoader/LandingLoader.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import './LandingLoader.css'
+import logoImage from '../Navigation/assets/svLogo.png';
+
+function LandingLoader() {
+    return (
+        <div id="landing-loader-wrapper">
+            <div id="landing-loader-main">
+                <div id="inside-div-1">
+                    <div>
+                        <div>
+                            <span>
+                                <img id="lander-logo-img" src={logoImage} />
+                            </span>
+                        </div>
+                    </div>
+                </div>
+
+                <div id="inside-div-2">
+                    <section>
+                        <h5>Sig Verify</h5>
+                        <p>Lorem ipsum dolor sit consectetur, velit!</p>
+                    </section>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default LandingLoader
